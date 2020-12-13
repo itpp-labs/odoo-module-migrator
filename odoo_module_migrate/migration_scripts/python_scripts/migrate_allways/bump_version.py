5b29bc5abcad9c1c05695ca4f57a85c789faa5e9
Ivan Yelizariev <iel@odoo.com>
2020-12-13 18:16:51 +0100

    change odoo version only

--- odoo_module_migrate/migration_scripts/python_scripts/migrate_allways/bump_version.py
+++ odoo_module_migrate/migration_scripts/python_scripts/migrate_allways/bump_version.py
@@ -1,13 +1,11 @@
 def bump_revision(**kwargs):
     tools = kwargs['tools']
     manifest_path = kwargs['manifest_path']
-    migration_steps = kwargs['migration_steps']
-    target_version_name = migration_steps[-1]["target_version_name"]
+    target_version_name = kwargs['migration_steps'][-1]["target_version_name"]
 
-    new_version = "%s.1.0.0" % target_version_name
+    import re
+    text = tools._read_content(manifest_path)
+    old_term = r"('|\")version('|\").*('|\")[0-9]+\.[0-9]+(.*)('|\")"
+    text = re.sub(old_term, r'"version": "{0}\4"'.format(target_version_name), text)
 
-    old_term = r"('|\")version('|\").*('|\").*('|\")"
-    new_term = r'\1version\2: "{0}"'.format(new_version)
-    tools._replace_in_file(
-        manifest_path, {old_term: new_term},
-        "Bump version to %s" % new_version)
+    tools._write_content(manifest_path, text)

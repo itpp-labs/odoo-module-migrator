be5ca6c662a36ad550966fc6983cee1679d55e39
Ivan Yelizariev // IEL <yelizariev@itpp.dev>
2020-12-13 18:28:46 +0100

    fixup

--- odoo_module_migrate/migration_scripts/migrate_allways.py
+++ odoo_module_migrate/migration_scripts/migrate_allways.py
@@ -20,7 +20,7 @@ def bump_revision(**kwargs):
     import re
     text = tools._read_content(manifest_path)
     old_term = r"('|\")version('|\").*('|\")[0-9]+\.[0-9]+(.*)('|\")"
-    text = re.sub(old_term, r'"version": "{0}\4""'.format(target_version_name), text)
+    text = re.sub(old_term, r'"version": "{0}\4"'.format(target_version_name), text)
 
     tools._write_content(manifest_path, text)
 
